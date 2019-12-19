6f5eaba900850e73a472958d48f174606975a32e
Rémi Bardon <remi.bardon.dev@gmail.com>
2019-12-19 11:42:14 +0100

    Initial Commit

---
+++ Alertes Meteo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Alertes Meteo
+//
+//  Created by BARDON Rémi on 19/12/2019.
+//  Copyright © 2019 ULR ECI A1-2. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
