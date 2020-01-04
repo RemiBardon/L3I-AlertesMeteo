e952f9ae20bdfdbe54dfd0055be689131a574d07
Rémi Bardon <remi.bardon.dev@gmail.com>
2020-01-04 16:17:16 +0100

    Ajout de la demande d'autorisation à envoyer des notifications.

--- Alertes Meteo/AlertsViewController.swift
+++ Alertes Meteo/AlertsViewController.swift
@@ -14,6 +14,50 @@ class AlertsViewController: UIViewController {
 		title = "Alertes"
 		
 		view.backgroundColor = .systemBackground
+		
+		configureNotificationCenter()
+	}
+	
+	private func configureNotificationCenter() {
+		// Asking Permission to Use Notifications:
+		// https://developer.apple.com/documentation/usernotifications/asking_permission_to_use_notifications
+		
+		let center = UNUserNotificationCenter.current()
+		center.getNotificationSettings { settings in
+			#if DEBUG
+			print("\(type(of: self)).\(#function): notificationSettings.authorizationStatus=", terminator: "")
+			switch settings.authorizationStatus {
+			case .authorized: 		print(".authorized")
+			case .denied: 			print(".denied")
+			case .notDetermined: 	print(".notDetermined")
+			case .provisional: 		print(".provisional")
+			@unknown default: 		print("@unknown")
+			}
+			#endif
+			
+			guard settings.authorizationStatus == .authorized else {
+				center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+					// Enable or disable features based on authorization.
+				}
+				return
+			}
+			
+			#if DEBUG
+			print("\(type(of: self)).\(#function): notificationSettings.alertSetting=", terminator: "")
+			switch settings.alertSetting {
+			case .enabled: 			print(".enabled")
+			case .disabled: 		print(".disabled")
+			case .notSupported: 	print(".notSupported")
+			@unknown default: 		print("@unknown")
+			}
+			#endif
+			
+			if settings.alertSetting == .enabled {
+				// Schedule an alert-only notification.
+			} else {
+				// Schedule a notification with a badge and sound.
+			}
+		}
 	}
 	
 }
