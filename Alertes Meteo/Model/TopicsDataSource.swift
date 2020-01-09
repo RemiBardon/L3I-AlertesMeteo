cf0972568d4272265fa1277aade0f7d5f527dc60
Rémi Bardon <remi.bardon.dev@gmail.com>
2020-01-09 15:23:17 +0100

    Optimisation de TopicsDataSource.

--- Alertes Meteo/Model/TopicsDataSource.swift
+++ Alertes Meteo/Model/TopicsDataSource.swift
@@ -39,13 +39,13 @@ class TopicsDataSource: ObservableObject {
 				
 				let db = Firestore.firestore()
 				
-				for topic in topicSubscriptions {
-					if !self.topics.contains(where: { $0.name == topic }) {
-						self.topics.append(Topic(name: topic))
+				for topicName in topicSubscriptions {
+					if !self.topics.contains(where: { $0.name == topicName }) {
+						self.topics.append(Topic(name: topicName))
 					}
 					self.topicListeners.append(
 						db.collection("alerts")
-							.whereField("topic", isEqualTo: topic)
+							.whereField("topic", isEqualTo: topicName)
 							.order(by: "timestamp", descending: true)
 							.limit(to: 5)
 							.addSnapshotListener { [weak self] (querySnapshot, error) in
@@ -58,6 +58,20 @@ class TopicsDataSource: ObservableObject {
 								guard let querySnapshot = querySnapshot else {
 									print("\(type(of: self)).\(#function): Error fetching documents: querySnapshot=nil")
 									return
+								}
+								
+								let topicIndex: Int
+								if let index = topicSubscriptions.firstIndex(of: topicName) {
+									topicIndex = index
+								} else {
+									self.topics.append(Topic(name: topicName))
+									topicIndex = self.topics.count - 1
+								}
+								
+								let topic = self.topics[topicIndex]
+								var alertIndexes = [String: Int]()
+								for i in 0..<topic.alerts.count {
+									alertIndexes[topic.alerts[i].id] = i
 								}
 								
 								for diff in querySnapshot.documentChanges {
@@ -76,21 +90,18 @@ class TopicsDataSource: ObservableObject {
 									let data = diff.document.prepareForDecoding()
 									guard let newAlert = try? JSONDecoder().decode(Alert.self, fromJSONObject: data) else { continue }
 									
-									guard let topicIndex = topicSubscriptions.firstIndex(of: topic) else { continue }
-									
-									let existingIndex = self.topics[topicIndex].alerts.firstIndex(where: { $0.id == newAlert.id })
 									switch diff.type {
 									case .added, .modified:
-										if let index = existingIndex {
-											self.topics[topicIndex].alerts[index] = newAlert
-										} else if let index = self.topics[topicIndex].alerts.firstIndex(where: { newAlert.timestamp > $0.timestamp }) {
-											self.topics[topicIndex].alerts.insert(newAlert, at: index)
+										if let index = alertIndexes[newAlert.id] {
+											topic.alerts[index] = newAlert
+										} else if let index = topic.alerts.firstIndex(where: { newAlert.timestamp > $0.timestamp }) {
+											topic.alerts.insert(newAlert, at: index)
 										} else {
-											self.topics[topicIndex].alerts.append(newAlert)
+											topic.alerts.append(newAlert)
 										}
 									case .removed:
-										if let index = existingIndex {
-											self.topics[topicIndex].alerts.remove(at: index)
+										if let index = alertIndexes[newAlert.id] {
+											topic.alerts.remove(at: index)
 										}
 									}
 								}
