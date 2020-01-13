f960b77829b3a213c630d279bce5ecd60bda5739
Rémi Bardon <remi.bardon.dev@gmail.com>
2020-01-13 21:34:12 +0100

    Ajout d'une extension permettant de décoder des données JSON dont la structure n'est pas connue à l'avance.

--- Alertes Meteo/Extensions/Foundation+Ext.swift
+++ Alertes Meteo/Extensions/Foundation+Ext.swift
@@ -24,3 +24,60 @@ extension UserDefaults {
     }
 	
 }
+
+// https://stackoverflow.com/a/47616252/10967642
+struct UnknownDataCodingKeys: CodingKey {
+	
+    var stringValue: String
+    init?(stringValue: String) {
+        self.stringValue = stringValue
+    }
+
+    var intValue: Int?
+    init?(intValue: Int) {
+        return nil
+    }
+	
+}
+
+extension KeyedDecodingContainer where Key == UnknownDataCodingKeys {
+	
+	/// Method used to decode unknown key-value data from an object
+	///
+	/// Usage:
+	/// ```
+	/// // Unknown data decoding
+	/// let container2 = try decoder.container(keyedBy: UnknownDataCodingKeys.self)
+	/// unknownData = container2.decodeUnknownKeyValues(exclude: CodingKeys.self)
+	///
+	/// // Nested unknown data decoding
+	/// let container3 = try container.nestedContainer(keyedBy: UnknownDataCodingKeys.self, forKey: .unknownNestedData)
+	/// unknownNestedData = container3.decodeUnknownKeyValues(exclude: CodingKeys.self)
+	/// ```
+	///
+	/// - Parameter keyedBy: Coding keys to exclude
+    func decodeUnknownKeyValues<T: CodingKey>(exclude keyedBy: T.Type) -> [String: Any] {
+        var data = [String: Any]()
+
+        for key in allKeys {
+            if keyedBy.init(stringValue: key.stringValue) == nil {
+                if let value = try? decode(String.self, forKey: key) {
+                    data[key.stringValue] = value
+                } else if let value = try? decode(Bool.self, forKey: key) {
+                    data[key.stringValue] = value
+                } else if let value = try? decode(Int.self, forKey: key) {
+                    data[key.stringValue] = value
+                } else if let value = try? decode(Double.self, forKey: key) {
+                    data[key.stringValue] = value
+                } else if let value = try? decode(Float.self, forKey: key) {
+                    data[key.stringValue] = value
+                } else {
+                    NSLog("Key %@ type not supported", key.stringValue)
+                }
+            }
+        }
+
+        return data
+    }
+	
+}
